91800ad34354f62c26ce6c0021d89dcb23331212
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-02-14 21:49:52 +0530

    Update 2149 Rearrange Array Elements by Sign.cpp

--- 2149 Rearrange Array Elements by Sign.cpp
+++ 2149 Rearrange Array Elements by Sign.cpp
@@ -35,3 +35,21 @@
 // 1 <= |nums[i]| <= 105
 // nums consists of equal number of positive and negative integers.
 
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size(), e = 0, o = 1;
+        vector<int> ret(n);
+        for(int i = 0;i < n;i++){
+            if(nums[i] > 0){
+                ret[e] = nums[i];
+                e = e + 2;
+            }
+            else{
+                ret[o] = nums[i];
+                o += 2;
+            }
+        }
+        return ret;
+    }
+};
