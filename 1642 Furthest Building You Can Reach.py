af8568a7d0101d73380ff88e663531cbd9a97bfa
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-02-17 22:52:18 +0530

    Update 1642 Furthest Building You Can Reach.py

--- 1642 Furthest Building You Can Reach.py
+++ 1642 Furthest Building You Can Reach.py
@@ -39,3 +39,17 @@
 # 1 <= heights[i] <= 106
 # 0 <= bricks <= 109
 # 0 <= ladders <= heights.length
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        h = []
+        for i, a in enumerate(heights[:-1]):
+            b = heights[i + 1]
+            d = b - a
+            if d > 0:
+                heappush(h, d)
+                if len(h) > ladders:
+                    bricks -= heappop(h)
+                    if bricks < 0:
+                        return i
+        return len(heights) - 1
