fe67e019ca261a2ec7e02c1bebbd7f1151fea9c5
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-02-21 23:23:50 +0530

    Update 201 Bitwise AND of Numbers Range.cpp

--- 201 Bitwise AND of Numbers Range.cpp
+++ 201 Bitwise AND of Numbers Range.cpp
@@ -20,3 +20,15 @@
 
 // 0 <= left <= right <= 231 - 1
   
+class Solution {
+public:
+  int rangeBitwiseAnd(int left, int right) {
+    int shift = 0;
+    while (left < right) {
+      left >>= 1;
+      right >>= 1;
+      shift++;
+    }
+    return left << shift;
+  }
+};
