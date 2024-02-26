021df5b129d48902587e9d28ec8bfab5fa72e628
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-02-26 20:28:50 +0530

    Update 100 Same Tree.cpp

--- 100 Same Tree.cpp
+++ 100 Same Tree.cpp
@@ -27,3 +27,42 @@
 
 // The number of nodes in both trees is in the range [0, 100].
 // -104 <= Node.val <= 104
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void preorder(TreeNode* p, TreeNode* q ,bool &flag){
+    if(!p && !q)return;
+    else if(!p&&q || !q&&p){
+       flag=false;
+        return; 
+    }
+
+    if(p->val!=q->val){
+        flag=false;
+        return;
+    }
+    preorder(p->left,q->left,flag);
+    preorder(p->right,q->right,flag);
+
+}
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        bool flag=true;
+        preorder(p,q,flag);
+        if(flag)return true;
+        return false;
+
+
+
+    }
+};
