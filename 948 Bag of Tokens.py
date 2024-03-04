c3aa63da58fd155b4007d8f51c44c044020121f3
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-03-04 22:42:43 +0530

    Update 948 Bag of Tokens.py

--- 948 Bag of Tokens.py
+++ 948 Bag of Tokens.py
@@ -49,3 +49,23 @@
 # 0 <= tokens.length <= 1000
 # 0 <= tokens[i], power < 104
 
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        l,r = 0,len(tokens)-1
+        score=0
+        tokens.sort()
+
+        while l<=r:
+            if power>=tokens[l]:
+                score+=1
+                power-=tokens[l]
+                l+=1
+            elif l<r and score>0:
+                score-=1
+                power+=tokens[r]
+                r-=1
+            else:
+                return score
+
+        return score
+                
