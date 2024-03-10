a5e660ef21bdcd4abaf3258cc554ed728faae353
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-03-10 21:53:24 +0530

    Update 349. Intersection of Two Arrays.py

--- 349. Intersection of Two Arrays.py
+++ 349. Intersection of Two Arrays.py
@@ -2,8 +2,6 @@
 
 # Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 
- 
-
 # Example 1:
 
 # Input: nums1 = [1,2,2,1], nums2 = [2,2]
@@ -13,9 +11,23 @@
 # Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 # Output: [9,4]
 # Explanation: [4,9] is also accepted.
- 
 
 # Constraints:
 
 # 1 <= nums1.length, nums2.length <= 1000
 # 0 <= nums1[i], nums2[i] <= 1000
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        mp = {}
+        for num in nums1:
+            mp[num] = mp.get(num, 0) + 1
+        
+        result = []
+        for num in nums2:
+            if num in mp:
+                result.append(num)
+                del mp[num]
+        
+        return result
+
