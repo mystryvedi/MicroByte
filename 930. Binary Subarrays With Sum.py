9da02b6fb06289e7efa3616c4aec07c147ab7a7a
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-03-14 23:37:05 +0530

    Update 930. Binary Subarrays With Sum.py

--- 930. Binary Subarrays With Sum.py
+++ 930. Binary Subarrays With Sum.py
@@ -4,8 +4,6 @@
 
 # A subarray is a contiguous part of the array.
 
- 
-
 # Example 1:
 
 # Input: nums = [1,0,1,0,1], goal = 2
@@ -20,9 +18,22 @@
 # Input: nums = [0,0,0,0,0], goal = 0
 # Output: 15
  
-
 # Constraints:
 
 # 1 <= nums.length <= 3 * 104
 # nums[i] is either 0 or 1.
 # 0 <= goal <= nums.length
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        count = {0: 1}
+        curr_sum = 0
+        total_subarrays = 0
+        
+        for num in nums:
+            curr_sum += num
+            if curr_sum - goal in count:
+                total_subarrays += count[curr_sum - goal]
+            count[curr_sum] = count.get(curr_sum, 0) + 1
+
+        return total_subarrays
