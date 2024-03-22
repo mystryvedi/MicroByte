95a82a6ffc5d474844a94502184905a9c700c7af
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-03-22 23:18:25 +0530

    Update 234. Palindrome Linked List.py

--- 234. Palindrome Linked List.py
+++ 234. Palindrome Linked List.py
@@ -17,3 +17,31 @@
 
 # The number of nodes in the list is in the range [1, 105].
 # 0 <= Node.val <= 9
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return True
+        slow = head
+        fast = head
+        prev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        if fast:
+            slow = slow.next
+        while prev and slow:
+            if prev.val != slow.val:
+                return False
+            prev = prev.next
+            slow = slow.next
+
+        return True
