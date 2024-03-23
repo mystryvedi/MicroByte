12fe0d500ca9ad590f8fa13722b021475770b944
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-03-23 22:20:57 +0530

    Update 143. Reorder List.py

--- 143. Reorder List.py
+++ 143. Reorder List.py
@@ -21,3 +21,33 @@
 
 # The number of nodes in the list is in the range [1, 5 * 104].
 # 1 <= Node.val <= 1000
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow , fast = head, head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev, current = None, slow.next
+        slow.next = None
+
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        parent_node, child_node = head, prev
+        while child_node:
+            next_node = parent_node.next
+            parent_node.next = child_node
+            parent_node = child_node
+            child_node = next_node
+        
+        return head
