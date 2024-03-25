a0e153c53fa51c9b06b96dccdd7d8539aae0855e
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-03-25 12:33:37 +0530

    Update 442. Find All Duplicates in an Array.py

--- 442. Find All Duplicates in an Array.py
+++ 442. Find All Duplicates in an Array.py
@@ -23,3 +23,14 @@
 # 1 <= n <= 105
 # 1 <= nums[i] <= n
 # Each element in nums appears once or twice.
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        ans =[]
+        n=len(nums)
+        for x in nums:
+            x = abs(x)
+            if nums[x-1]<0:
+                ans.append(x)
+            nums[x-1] *= -1
+        return ans
