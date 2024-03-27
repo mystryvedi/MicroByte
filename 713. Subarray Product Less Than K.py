706a239898c7249f343dd8b9f1735b8ac0aaefe1
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-03-27 22:04:51 +0530

    Update 713. Subarray Product Less Than K.py

--- 713. Subarray Product Less Than K.py
+++ 713. Subarray Product Less Than K.py
@@ -19,3 +19,21 @@
 # 1 <= nums.length <= 3 * 104
 # 1 <= nums[i] <= 1000
 # 0 <= k <= 106
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+
+        left, right, product, count = 0, 0, 1, 0
+        n = len(nums)
+
+        while right < n:
+            product *= nums[right]
+            while product >= k:
+                product //= nums[left]
+                left += 1
+            count += 1 + (right - left)
+            right += 1
+
+        return count
