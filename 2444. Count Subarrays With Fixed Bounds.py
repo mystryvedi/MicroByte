96691c39ba8ab2be08db5d8b9f60cbc6f8bf9fb4
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-03-31 20:34:45 +0530

    Update 2444. Count Subarrays With Fixed Bounds.py

--- 2444. Count Subarrays With Fixed Bounds.py
+++ 2444. Count Subarrays With Fixed Bounds.py
@@ -25,3 +25,28 @@
 
 # 2 <= nums.length <= 105
 # 1 <= nums[i], minK, maxK <= 106
+
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        n = len(nums)
+        result = 0
+        minKIndex = -1
+        maxKIndex = -1
+        culpritIndex = -1
+
+        for i in range(n):
+            if nums[i] < minK or nums[i] > maxK:
+                culpritIndex = i
+
+            if nums[i] == minK:
+                minKIndex = i
+
+            if nums[i] == maxK:
+                maxKIndex = i
+
+            smaller = min(minKIndex, maxKIndex)
+            temp = smaller - culpritIndex
+
+            result += temp if temp > 0 else 0
+
+        return result
