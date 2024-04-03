120d2e2bb4a92752fd768670c6b67af962ef204e
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-03 20:05:55 +0530

    Update 79. Word Search.py

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -24,3 +24,26 @@
 # 1 <= m, n <= 6
 # 1 <= word.length <= 15
 # board and word consists of only lowercase and uppercase English letters.
+
+class Solution:
+    def exist(self, board, word):
+        def backtrack(i, j, k):
+            if k == len(word):
+                return True
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k]:
+                return False
+            
+            temp = board[i][j]
+            board[i][j] = ''
+            
+            if backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j+1, k+1) or backtrack(i, j-1, k+1):
+                return True
+            
+            board[i][j] = temp
+            return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtrack(i, j, 0):
+                    return True
+        return False
