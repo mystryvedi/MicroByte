7222ebe2f6d8db9dcd0a0babcb46afa265383299
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-04 20:38:21 +0530

    Update 1614. Maximum Nesting Depth of the Parentheses.py

--- 1614. Maximum Nesting Depth of the Parentheses.py
+++ 1614. Maximum Nesting Depth of the Parentheses.py
@@ -30,3 +30,16 @@
 # 1 <= s.length <= 100
 # s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
 # It is guaranteed that parentheses expression s is a VPS.
+
+class Solution:
+    def maxDepth(self, s):
+        count = 0
+        max_num = 0
+        for i in s:
+            if i == "(":
+                count += 1
+                if max_num < count:
+                    max_num = count
+            if i == ")":
+                count -= 1
+        return(max_num)
