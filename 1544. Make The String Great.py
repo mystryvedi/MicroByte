20f6ae1bb93854f60765ea06cab37581e32cafc7
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-05 21:04:56 +0530

    Update 1544. Make The String Great.py

--- 1544. Make The String Great.py
+++ 1544. Make The String Great.py
@@ -33,3 +33,14 @@
 
 # 1 <= s.length <= 100
 # s contains only lower and upper case English letters.
+
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []  
+        for char in s:
+            if stack and abs(ord(char) - ord(stack[-1])) == 32:
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return ''.join(stack)
