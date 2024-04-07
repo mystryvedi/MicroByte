62b621d1d053ab8dddf08cee8305418675e91bad
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-07 23:09:52 +0530

    Update 678. Valid Parenthesis String.py

--- 678. Valid Parenthesis String.py
+++ 678. Valid Parenthesis String.py
@@ -26,3 +26,20 @@
 
 # 1 <= s.length <= 100
 # s[i] is '(', ')' or '*'.
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftMin, leftMax = 0, 0
+
+        for c in s:
+            if c == "(":
+                leftMin, leftMax = leftMin + 1, leftMax + 1
+            elif c == ")":
+                leftMin, leftMax = leftMin - 1, leftMax - 1
+            else:
+                leftMin, leftMax = leftMin - 1, leftMax + 1
+            if leftMax < 0:
+                return False
+            if leftMin < 0:
+                leftMin = 0
+        return leftMin == 0
