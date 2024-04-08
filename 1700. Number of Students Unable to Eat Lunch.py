24b9cce25657217651b65fd21eb91e20dd172682
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-08 19:51:35 +0530

    Update 1700. Number of Students Unable to Eat Lunch.py

--- 1700. Number of Students Unable to Eat Lunch.py
+++ 1700. Number of Students Unable to Eat Lunch.py
@@ -35,3 +35,20 @@
 # students.length == sandwiches.length
 # sandwiches[i] is 0 or 1.
 # students[i] is 0 or 1.
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        counts = [0, 0]
+        for student in students:
+            counts[student] += 1
+
+        remaining = len(sandwiches)
+        for sandwich in sandwiches:
+            if counts[sandwich] == 0:
+                break
+            if remaining == 0:
+                break
+            counts[sandwich] -= 1
+            remaining -= 1
+        
+        return remaining
