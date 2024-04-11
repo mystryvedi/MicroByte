2c383feb93321fb6c7044a38911e03f03cb6681e
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-11 10:02:41 +0530

    Update 402. Remove K Digits.py

--- 402. Remove K Digits.py
+++ 402. Remove K Digits.py
@@ -23,3 +23,14 @@
 # 1 <= k <= num.length <= 105
 # num consists of only digits.
 # num does not have any leading zeros except for the zero itself.
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stk = []
+        remain = len(num) - k
+        for c in num:
+            while k and stk and stk[-1] > c:
+                stk.pop()
+                k -= 1
+            stk.append(c)
+        return ''.join(stk[:remain]).lstrip('0') or '0'
