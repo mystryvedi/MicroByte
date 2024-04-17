d8651245b1ee428e7843660cc4cc34c3485731b4
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-17 23:05:04 +0530

    Update 988. Smallest String Starting From Leaf.py

--- 988. Smallest String Starting From Leaf.py
+++ 988. Smallest String Starting From Leaf.py
@@ -26,3 +26,24 @@
 
 # The number of nodes in the tree is in the range [1, 8500].
 # 0 <= Node.val <= 25
+
+class Solution:
+    def smallestFromLeaf(self, root: TreeNode) -> str:
+        self.ans = None
+        self.dfs(root, [])
+        return self.ans
+
+    def dfs(self, root, path):
+        if not root:
+            return
+
+        path.append(chr(root.val + ord('a')))
+
+        if not root.left and not root.right:
+            rev_path = ''.join(path[::-1])
+            if self.ans is None or rev_path < self.ans:
+                self.ans = rev_path
+
+        self.dfs(root.left, path)
+        self.dfs(root.right, path)
+        path.pop()
