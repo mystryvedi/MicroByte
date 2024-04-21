82a4bd1da8cc5a4029ae6e8a9edca4d4ff255103
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-21 10:27:09 +0530

    Update 1971. Find if Path Exists in Graph.py

--- 1971. Find if Path Exists in Graph.py
+++ 1971. Find if Path Exists in Graph.py
@@ -29,3 +29,23 @@
 # 0 <= source, destination <= n - 1
 # There are no duplicate edges.
 # There are no self edges.
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = collections.defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        def dfs(node, visited):
+            if node == destination:
+                return True
+            visited.add(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    if dfs(neighbor, visited):
+                        return True
+            return False
+        
+        visited = set()
+        return dfs(source, visited)
