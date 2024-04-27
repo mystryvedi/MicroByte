7e42b408d35a1c1529731d24f4fbd87f8715bea4
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-04-27 22:21:57 +0530

    Update 514. Freedom Trail.py

--- 514. Freedom Trail.py
+++ 514. Freedom Trail.py
@@ -30,3 +30,21 @@
 # 1 <= ring.length, key.length <= 100
 # ring and key consist of only lower case English letters.
 # It is guaranteed that key could always be spelled by rotating ring.
+
+class Solution:
+  def findRotateSteps(self, ring: str, key: str) -> int:
+    @functools.lru_cache(None)
+    def dfs(ring: str, index: int) -> int:
+      if index == len(key):
+        return 0
+      ans = math.inf
+      for i, r in enumerate(ring):
+        if r == key[index]:
+          minRotates = min(i, len(ring) - i)
+          newRing = ring[i:] + ring[:i]
+          remainingRotates = dfs(newRing, index + 1)
+          ans = min(ans, minRotates + remainingRotates)
+
+      return ans
+
+    return dfs(ring, 0) + len(key)
