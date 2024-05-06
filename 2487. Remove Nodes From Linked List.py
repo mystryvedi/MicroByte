adc730152d620c86e8c4bb132c4e05c6cf54ed98
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-06 20:04:36 +0530

    Update 2487. Remove Nodes From Linked List.py

--- 2487. Remove Nodes From Linked List.py
+++ 2487. Remove Nodes From Linked List.py
@@ -24,3 +24,26 @@
 
 # The number of the nodes in the given list is in the range [1, 105].
 # 1 <= Node.val <= 105
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        stack = []
+        while cur:
+            while stack and stack[-1].val < cur.val:
+                stack.pop()
+            stack.append(cur)
+            cur = cur.next
+        
+        nxt = None
+        while stack:
+            cur = stack.pop()
+            cur.next = nxt
+            nxt = cur
+        
+        return cur
