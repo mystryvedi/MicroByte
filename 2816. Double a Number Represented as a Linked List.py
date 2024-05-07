5b91c6b9446aa7504b6c05cb8e2beb6f6a1fa4e0
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-07 09:27:31 +0530

    Update 2816. Double a Number Represented as a Linked List.py

--- 2816. Double a Number Represented as a Linked List.py
+++ 2816. Double a Number Represented as a Linked List.py
@@ -20,3 +20,22 @@
 # The number of nodes in the list is in the range [1, 104]
 # 0 <= Node.val <= 9
 # The input is generated such that the list represents a number that does not have leading zeros, except the number 0 itself.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def func(head):
+            a=head.val*2
+            if head.next:
+                a+=func(head.next)
+            head.val=a%10
+            return a//10
+        a=func(head)
+        if a:
+            return ListNode(a,head)
+        return head
+        
