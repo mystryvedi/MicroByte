2e59a2c59f7638b5de73dea397aaa765698e92c6
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-08 20:38:07 +0530

    Update 506. Relative Ranks.py

--- 506. Relative Ranks.py
+++ 506. Relative Ranks.py
@@ -27,3 +27,10 @@
 # 1 <= n <= 104
 # 0 <= score[i] <= 106
 # All the values in score are unique
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        sorted_score = sorted(score, reverse=True)
+        medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+        rank_mapping = {score: medals[i] if i < 3 else str(i + 1) for i, score in enumerate(sorted_score)}
+        return [rank_mapping[score] for score in score]
