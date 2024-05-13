2483587688c59ef3c5aa6c0660292cb0118b25bc
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-13 16:07:14 +0530

    Update 861. Score After Flipping Matrix.py

--- 861. Score After Flipping Matrix.py
+++ 861. Score After Flipping Matrix.py
@@ -24,3 +24,20 @@
 # n == grid[i].length
 # 1 <= m, n <= 20
 # grid[i][j] is either 0 or 1.
+
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        res = (1 << (m - 1)) * n
+
+        for j in range(1, m):
+            val = 1 << (m - 1 - j)
+            set_count = 0
+
+            for i in range(n):
+                if grid[i][j] == grid[i][0]:
+                    set_count += 1
+
+            res += max(set_count, n - set_count) * val
+
+        return res
