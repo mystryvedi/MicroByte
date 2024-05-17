121502de81bf55185100825fdb4ec94dcee2e9d5
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-17 20:11:52 +0530

    Update 1325. Delete Leaves With a Given Value.py

--- 1325. Delete Leaves With a Given Value.py
+++ 1325. Delete Leaves With a Given Value.py
@@ -25,3 +25,23 @@
 # The number of nodes in the tree is in the range [1, 3000].
 # 1 <= Node.val, target <= 1000
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        def postorder(root, target):
+            if root==None: return None
+            
+            root.left=postorder(root.left, target)
+            root.right=postorder(root.right, target)
+
+            if root.left==root.right and root.val==target:
+                return None
+            return root
+            
+        return postorder(root, target)
+        
