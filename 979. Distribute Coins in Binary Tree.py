b0638bb85d8d20159d4698b3d9bd98a1d0642e35
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-18 12:29:52 +0530

    Update 979. Distribute Coins in Binary Tree.py

--- 979. Distribute Coins in Binary Tree.py
+++ 979. Distribute Coins in Binary Tree.py
@@ -24,3 +24,19 @@
 # 0 <= Node.val <= n
 # The sum of all Node.val is n.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        def f(root, parent):
+            if root==None: return 0
+            moves=f(root.left, root)+f(root.right, root)
+            x=root.val-1
+            if parent!=None: parent.val+=x
+            moves+=abs(x)
+            return moves
+        return f(root, None)
