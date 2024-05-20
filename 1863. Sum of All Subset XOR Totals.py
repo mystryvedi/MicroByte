44c5f12ba93d498f134dc893e73a495cec8150e1
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-20 21:56:54 +0530

    Update 1863. Sum of All Subset XOR Totals.py

--- 1863. Sum of All Subset XOR Totals.py
+++ 1863. Sum of All Subset XOR Totals.py
@@ -43,3 +43,13 @@
 
 # 1 <= nums.length <= 12
 # 1 <= nums[i] <= 20
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        return self.helper(nums, 0, 0) 
+    def helper(self, nums: List[int], level: int, currentXOR: int) -> int:
+        if level == len(nums):
+            return currentXOR
+        include = self.helper(nums, level + 1, currentXOR ^ nums[level])
+        exclude = self.helper(nums, level + 1, currentXOR)
+        return include + exclude
