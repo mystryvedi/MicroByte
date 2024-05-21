ccce92b7f9dc4d9c2db4bd51a5d96384d6547cd1
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-21 19:18:11 +0530

    Update 78. Subsets.py

--- 78. Subsets.py
+++ 78. Subsets.py
@@ -21,3 +21,13 @@
 # -10 <= nums[i] <= 10
 # All the numbers of nums are unique
 
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, path):
+            result.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+
+        result = []
+        backtrack(0, [])
+        return result
