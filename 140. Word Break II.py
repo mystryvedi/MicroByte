09d4b7d668ea74527d872f3be26884db6c02cc03
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-25 16:44:19 +0530

    Update 140. Word Break II.py

--- 140. Word Break II.py
+++ 140. Word Break II.py
@@ -27,3 +27,21 @@
 # All the strings of wordDict are unique.
 # Input is generated in a way that the length of the answer doesn't exceed 105.
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        word_set = set(wordDict)
+        return self.wordBreakHelper(s, 0, word_set)
+    
+    def wordBreakHelper(self, s: str, start: int, word_set: set) -> List[str]:
+        valid_substr = []
+        
+        if start == len(s):
+            valid_substr.append("")
+        for end in range(start + 1, len(s) + 1):
+            prefix = s[start:end]
+            if prefix in word_set:
+                suffixes = self.wordBreakHelper(s, end, word_set)
+                for suffix in suffixes:
+                    valid_substr.append(prefix + ("" if suffix == "" else " ") + suffix)
+        
+        return valid_substr
