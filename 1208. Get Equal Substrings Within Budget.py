9eb1e9b681607e2a18d77c42f51dac08b39fb32e
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-28 19:33:52 +0530

    Update 1208. Get Equal Substrings Within Budget.py

--- 1208. Get Equal Substrings Within Budget.py
+++ 1208. Get Equal Substrings Within Budget.py
@@ -30,3 +30,20 @@
 # 0 <= maxCost <= 106
 # s and t consist of only lowercase English letters.
 
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        n = len(s)
+        start = 0
+        current_cost = 0
+        max_length = 0
+
+        for end in range(n):
+            current_cost += abs(ord(s[end]) - ord(t[end]))
+
+            while current_cost > maxCost:
+                current_cost -= abs(ord(s[start]) - ord(t[start]))
+                start += 1
+
+            max_length = max(max_length, end - start + 1)
+        
+        return max_length
