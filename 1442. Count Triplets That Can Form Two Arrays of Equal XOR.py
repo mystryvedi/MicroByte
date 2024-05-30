8058b8f57def11a139308c947681dc59d2a23c5a
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-05-30 22:34:15 +0530

    Update 1442. Count Triplets That Can Form Two Arrays of Equal XOR.py

--- 1442. Count Triplets That Can Form Two Arrays of Equal XOR.py
+++ 1442. Count Triplets That Can Form Two Arrays of Equal XOR.py
@@ -27,3 +27,21 @@
 # 1 <= arr.length <= 300
 # 1 <= arr[i] <= 108
 
+class Solution:
+    def countTriplets(self, arr: List[int]) -> int:
+        count = 0
+        n = len(arr)
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                a = 0
+                for k in range(i, j):
+                    a ^= arr[k]
+                
+                b = 0
+                for k in range(j, n):
+                    b ^= arr[k]
+                    if a == b:
+                        count += 1
+        
+        return count
