d2d38937a9431e721269311865d19b627488ea71
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-06-01 21:14:44 +0530

    Update 3110. Score of a String.py

--- 3110. Score of a String.py
+++ 3110. Score of a String.py
@@ -29,3 +29,9 @@
 # 2 <= s.length <= 100
 # s consists only of lowercase English letters.
 
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        total = 0
+        for i in range(len(s) - 1):
+            total += abs(ord(s[i]) - ord(s[i + 1]))
+        return total
