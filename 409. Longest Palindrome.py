b2ba3bb377cff93ea81f8ffadb730553b7551a8e
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-06-04 20:11:21 +0530

    Update 409. Longest Palindrome.py

--- 409. Longest Palindrome.py
+++ 409. Longest Palindrome.py
@@ -22,3 +22,26 @@
 # 1 <= s.length <= 2000
 # s consists of lowercase and/or uppercase English letters only.
 
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hm = {}
+        for c in s:
+            if c in hm:
+                hm[c] += 1
+            else:
+                hm[c] = 1
+
+        ans = 0
+        isFirstOdd = False
+
+        for key in hm:
+            if hm[key] % 2 == 0:
+                ans += hm[key]
+            else:
+                ans += hm[key]
+                if not isFirstOdd:
+                    isFirstOdd = True
+                else:
+                    ans -= 1
+
+        return ans
