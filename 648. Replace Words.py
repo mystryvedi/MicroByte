06c960b1ac84223c0ae8577cf93af6adb3267084
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-06-07 11:12:01 +0530

    Update 648. Replace Words.py

--- 648. Replace Words.py
+++ 648. Replace Words.py
@@ -27,3 +27,43 @@
 # Every two consecutive words in sentence will be separated by exactly one space.
 # sentence does not have leading or trailing spaces.
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isEnd = True
+    
+    def search(self, word):
+        node = self.root
+        prefix = ''
+        for char in word:
+            if char not in node.children:
+                return word
+            prefix += char
+            if node.children[char].isEnd:
+                return prefix
+            node = node.children[char]
+        return word
+
+class Solution:
+    def replaceWords(self, dictionary, sentence):
+        trie = Trie()
+        for word in dictionary:
+            trie.insert(word)
+        
+        result = []
+        for word in sentence.split():
+            result.append(trie.search(word))
+        
+        return ' '.join(result)
