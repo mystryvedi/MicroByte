3eaf1825410959d2f2cbc8aabc8a7610c3a93b94
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2024-06-11 17:58:05 +0530

    Update 1122. Relative Sort Array.py

--- 1122. Relative Sort Array.py
+++ 1122. Relative Sort Array.py
@@ -20,3 +20,11 @@
 # All the elements of arr2 are distinct.
 # Each arr2[i] is in arr1
 
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        def rank(mp, a):
+            return mp.get(a, len(mp))
+    
+        mp = {num: i for i, num in enumerate(arr2)}
+        arr1.sort(key=lambda x: (rank(mp, x), x))
+        return arr1
